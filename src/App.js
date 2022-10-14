f29bf29ec7bfe14035d28a3a695f25bd36b22140
wubeshetA <w.yimam@alustudent.com>
2022-10-14 20:08:52 +0300

    add planets app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const planets = [
+    {name: "Mars", isGasplanet: false},
+    {name: "Earth", isGasplanet: false},
+    {name: "Jupiter", isGasplanet: true},
+    {name: "Venus", isGasplanet: false},
+    {name: "Neptune", isGasplanet: true},
+    {name: "Uranus", isGasplanet: true},
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {planets.map((planet, key) =>
+        planet.isGasplanet && <h1>{planet.name}</h1>
+      )}
     </div>
   );
+   
 }
 
 export default App;
