63d4bc4d03c87733d3740f043ac2f7172d747c6d
wubeshetA <w.yimam@alustudent.com>
2022-10-14 22:20:17 +0300

    add state implementation example code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import './App.css';
+import React from 'react';
+import {useState} from 'react';
 
 function App() {
-  const planets = [
-    {name: "Mars", isGasplanet: false},
-    {name: "Earth", isGasplanet: false},
-    {name: "Jupiter", isGasplanet: true},
-    {name: "Venus", isGasplanet: false},
-    {name: "Neptune", isGasplanet: true},
-    {name: "Uranus", isGasplanet: true},
-  ];
+  // implemente state 
+  let [value, setValue] = useState(0);
+
   return (
     <div className="App">
-      {planets.map((planet, key) =>
-        planet.isGasplanet && <h1>{planet.name}</h1>
-      )}
+      <button onClick={() => setValue(value+=1)}>Increase</button>
+      <button onClick={() => setValue(value-=1)}>Decrease</button>
+      <button onClick={() => setValue(0)}>setToZero</button>
+      {value}
     </div>
   );
-   
 }
 
 export default App;
